801722c9f55729dda524d226f21e4db5bb870bad
Chibgatullah Minhaz <chibgatullahminhaz@gmail.com>
2025-11-06 16:04:46 +0600

    delete

--- Backend/src/cron/banCron.js
+++
@@ -1,8 +0,0 @@
-import cron from "node-cron";
-import { clearExpiredBans } from "../app/modules/Users/users.service.js";
-
-// Schedule cron job to run every minute
-cron.schedule("* * * * *", () => {
-  console.log("Running cron job to clear expired bans at", new Date().toLocaleString());
-  clearExpiredBans();
-});
